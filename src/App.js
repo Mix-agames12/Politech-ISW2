fc6a62d56b7dadad0154fbfa3e927488d01db6da
domepl <dome.plaza456@gmail.com>
2024-07-16 19:30:12 -0500

    -App.js

--- src/App.js
+++ src/App.js
@@ -8,18 +8,12 @@ import UpdateProfile from './components/UpdateProfile';
 import Transaction from './components/Transaction';
 import AccountManagement from './components/AccountManagement';
 import Home from './components/Home';
-
-// import { Sidebar } from './components/Sidebar';
 import { GestionarCuentas } from './components/GestionarCuentas';
-
 
 function App() {
   return (
     <Router>
       <div className="App">
-      <Home /> {/* Usa el componente Home */}
-    </div>
-      <div className="container">
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
           <Link className="navbar-brand" to="/">Banking App</Link>
           <div className="collapse navbar-collapse">
@@ -39,9 +33,27 @@ function App() {
               <li className="nav-item">
                 <Link className="nav-link" to="/transaction">Transaction</Link>
               </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/account-management">Account Management</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/gestionar-cuentas">Gestionar Cuentas</Link>
+              </li>
             </ul>
           </div>
         </nav>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/password-reset" element={<PasswordReset />} />
+            <Route path="/update-profile" element={<UpdateProfile />} />
+            <Route path="/transaction" element={<Transaction />} />
+            <Route path="/account-management" element={<AccountManagement />} />
+            <Route path="/gestionar-cuentas" element={<GestionarCuentas />} />
+          </Routes>
+        </div>
       </div>
     </Router>
   );
