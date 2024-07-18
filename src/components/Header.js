d85c75de54b98280540ff494eb9203786ea79e55
Leonardo <leonardo.quishpe@epn.edu.ec>
2024-07-18 12:21:50 -0500

    Update styles in Login.css, package.json, Login.js, GestionarCuentas.css, SignUp.js, Sidebar.js, App.css, Sidebar.css, and index.css

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,29 +7,26 @@ import { verifyPasswordResetCode } from 'firebase/auth';
 export const Header = () => {
     return (
         <>
-            <nav className="navbar navbar-expand-lg bg-body- fixed-top" style={{ backgroundColor: '#061f3e' }}>
+            <nav className="navbar navbar-expand-lg bg-body- fixed-top" style={{ backgroundColor: '#061f3e', justifyContent: 'flex-end' }}>
                 <div className="container-fluid">
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     {/* <a className="navbar-brand" href="#">BANCO POLITECH</a> */}
-                    <img src={Logo} alt="Logo Politech" width="130" height="35" style={ {padding: '0'} }/>
-                    <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+                    <img src={Logo} alt="Logo Politech" width="120" height="35" style={{ padding: '3px' }} />
+                    <div className="collapse navbar-collapse justify-content-end" id="navbarTogglerDemo03">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#" style={{ color: 'white' }}>Home</a>
+                                <button className="nav-link active btn btn-link" aria-current="page" style={{ color: 'white' }}>Acceso a clientes</button>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#" style={{ color: 'white' }}>Link</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link disabled" aria-disabled="true" style={{ color: 'white' }}>Disabled</a>
+                                <button className="nav-link btn btn-link" style={{ color: 'white' }}>Abre una cuenta</button>
                             </li>
                         </ul>
-                        <form className="d-flex" role="search">
+                        {/* <form className="d-flex" role="search">
                             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
                             <button className="btn btn-outline-success" type="submit" style={{ color: 'white' }}>Search</button>
-                        </form>
+                        </form> */}
                     </div>
                 </div>
             </nav>
