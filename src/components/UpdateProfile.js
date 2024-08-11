a5ab8770daf9ef49c8669a5beb43c43ef37593b0
GLENN ISAAC SALCEDO ONA <glennsalcedo@hotmail.com>
2024-08-11 00:36:05 -0500

    modified:   src/components/UpdateProfile.js     can update username and password

--- src/components/UpdateProfile.js
+++ src/components/UpdateProfile.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { auth, db } from '../firebaseConfig';
 
-import { updateEmail, updatePassword, updateProfile, reauthenticateWithCredential, EmailAuthProvider } from 'firebase/auth';
-
+import { updatePassword, updateProfile } from 'firebase/auth';
+import { FaRegEye, FaRegEyeSlash, FaArrowLeft } from 'react-icons/fa';
 import { doc, getDoc, updateDoc } from 'firebase/firestore';
 import { Sidebar } from '../components/Sidebar';
 import { useNavigate } from 'react-router-dom'; // Importar useNavigate
@@ -10,7 +10,6 @@ import { HeaderDashboard } from './HeaderDashboard';
 
 const UpdateProfile = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-  const [email, setEmail] = useState('');
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
@@ -18,6 +17,10 @@ const UpdateProfile = () => {
   const [client, setClient] = useState(null);
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [passwordsMatch, setPasswordsMatch] = useState(true);
   const navigate = useNavigate(); // Usar useNavigate
   const [passwordConditions, setPasswordConditions] = useState({
     length: false,
@@ -65,20 +68,13 @@ const UpdateProfile = () => {
     const number = /[0-9]/.test(password);
     const specialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
 
-    const isValid = length && uppercase && number && specialChar;
-
     setPasswordConditions({ length, uppercase, number, specialChar });
     setPassword(password);
-    return isValid;
   };
 
-  const reauthenticateUser = async (currentUser, password) => {
-    const credential = EmailAuthProvider.credential(currentUser.email, password);
-    try {
-      await reauthenticateWithCredential(currentUser, credential);
-    } catch (error) {
-      setError('Reautenticación fallida. Por favor, intenta de nuevo.');
-    }
+  const handleConfirmPassword = (value) => {
+    setConfirmPassword(value);
+    setPasswordsMatch(value === password);
   };
 
   const handleUpdate = async () => {
@@ -86,32 +82,28 @@ const UpdateProfile = () => {
     setSuccess('');
     const currentUser = auth.currentUser;
 
-    let changesMade = false;
+    let usernameChanged = false;
+    let passwordChanged = false;
 
     if (currentUser) {
       try {
-        if (email && email !== user.correo) {
-          await reauthenticateUser(currentUser, password);
-          await updateEmail(currentUser, email);
-          await updateDoc(doc(db, 'clientes', currentUser.uid), { correo: email });
-          await updateDoc(doc(db, 'users', currentUser.uid), { correo: email });
-          changesMade = true;
-        }
         if (username && username !== user.username) {
           await updateProfile(currentUser, { displayName: username });
           await updateDoc(doc(db, 'users', currentUser.uid), { username });
-          changesMade = true;
+          usernameChanged = true;
+          setSuccess('Nombre de usuario actualizado correctamente');
         }
         if (password) {
-          if (!validatePassword(password)) {
-            setError('La contraseña no cumple con los requisitos.');
+          if (password !== confirmPassword) {
+            setError('Debes colocar la misma contraseña en ambos campos')
             return;
           }
           await updatePassword(currentUser, password);
-          changesMade = true;
+          passwordChanged = true;
+          setSuccess('Contraseña actualizada correctamente');
         }
-        if (changesMade) {
-          setSuccess('Perfil actualizado correctamente');
+        if (usernameChanged && passwordChanged) {
+          setSuccess('Datos actualizados correctamente');
         }
       } catch (error) {
         setError('Error al actualizar el perfil. Por favor, intenta de nuevo.');
@@ -126,73 +118,116 @@ const UpdateProfile = () => {
 
   return (
     <div className="min-h-screen flex">
-      <HeaderDashboard/>
+      <HeaderDashboard />
       <div className="flex flex-grow w-full">
         <div className="w-1/4">
           <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
         </div>
         <div className="mx-auto flex flex-col items-center justify-end w-2/5 bg-gray-100">
-            <div className="w-full p-8">
-              <div className="text-center mb-8">
-                <h2 className="text-3xl font-bold text-gray-900">MI PERFIL</h2>
+          <div className="w-full p-8">
+            <div className="text-center mb-8">
+              <h2 className="text-3xl font-bold text-gray-900">MI PERFIL</h2>
+            </div>
+            <h2 className="mb-4 text-black font-bold text-lg">Información personal</h2>
+            <div className="border border-gray-300 rounded-md p-4 mb-6">
+              <div className="mb-4">
+                <label className="block text-sm font-medium text-gray-700 mb-2">Nombre</label>
+                <h3 className="w-full px-4 py-2">{client.nombre} {client.apellido}</h3>
               </div>
-              <h2 className="mb-4 text-black font-bold text-lg">Información personal</h2>
-              <div className="border border-gray-300 rounded-md p-4 mb-6">
-                <div className="mb-4">
-                  <label className="block text-sm font-medium text-gray-700 mb-2">Nombre</label>
-                <h3 className="w-full px-4 py-2">{ client.nombre } { client.apellido }</h3>
-                </div>
-                <div className="mb-4">
-                  <label className="block text-sm fclientmedium text-gray-700 mb-2">Cédula</label>
-                <h3 className="w-full px-4 py-2">{ client.cedula }</h3>
-                </div>
-                <div>
-                  <label className="block text-sm fclientmedium text-gray-700 mb-2">Fecha de nacimiento</label>
-                <h3 className="w-full px-4 py-2">{ client.fechaNacimiento }</h3>
-                </div>
+              <div className="mb-4">
+                <label className="block text-sm fclientmedium text-gray-700 mb-2">Cédula</label>
+                <h3 className="w-full px-4 py-2">{client.cedula}</h3>
               </div>
-
-              <h2 className="mb-4 text-black font-bold text-lg">Mantén actualizada tu información</h2>
-              <div className="border border-gray-300 rounded-md p-4 mb-6">
-                <div className="mb-4">
-                  <label className="block text-sm font-medium text-gray-700 mb-2">Nombre de usuario</label>
-                  <input
-                    type="text"
-                    className="w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                    placeholder = { user.username }
-                    onChange={(e) => setUsername(e.target.value)}
-                  />
-                </div>
-                <div className="mb-4">
-                  <label className="block text-sm font-medium text-gray-700 mb-2">Correo Electrónico</label>
-                  <input
-                    type="email"
-                    className="w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                    placeholder = { user.correo }
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                </div>
-                <div className="mb-6">
-                  <label className="block text-sm font-medium text-gray-700 mb-2">Contraseña</label>
-                  <input
-                    type="password"
-                    className="w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                    placeholder = "Ingresa una nueva contraseña"
-                    onChange={(e) => validatePassword(e.target.value)}
-                  />
-                </div>
-                {error && <label className="block text-red-600 mb-4">{error}</label>}
-                {success && <label className="block text-green-600 mb-4 text-center">{success}</label>}
-                <div className="text-center">
-                  <input
-                    className="bg-sky-900 text-white px-4 py-2 rounded-md shadow-sm hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                    type="button"
-                    onClick={handleUpdate}
-                    value="Actualizar datos"
-                  />
-                </div>
+              <div>
+                <label className="block text-sm fclientmedium text-gray-700 mb-2">Fecha de nacimiento</label>
+                <h3 className="w-full px-4 py-2">{client.fechaNacimiento}</h3>
               </div>
             </div>
+
+            <h2 className="mb-4 text-black font-bold text-lg">Mantén actualizada tu información</h2>
+            <div className="border border-gray-300 rounded-md p-4 mb-6">
+              <div className="mb-4">
+                <label className="block text-sm font-medium text-gray-700 mb-2">Nombre de usuario</label>
+                <input
+                  type="text"
+                  className="w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  placeholder={user.username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </div>
+              <div className="grid grid-cols-2  gap-3 ">
+                <label className=" block text-sm font-medium text-gray-700">Nueva contraseña</label>
+                <label className=" block text-sm font-medium text-gray-700">Nueva contraseña</label>
+                <div className="flex items-center relative">
+                  <input
+                    id="password"
+                    name="password"
+                    type={showPassword ? "text" : "password"}
+                    value={password}
+                    onChange={(e) => validatePassword(e.target.value)}
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    placeholder="Contraseña"
+                    required
+                  />
+                  <button
+                    type="button"
+                    className="absolute right-0 pr-3 flex items-center top-1/2 transform -translate-y-1/2"
+                    onClick={() => setShowPassword(!showPassword)}
+                  >
+                    {showPassword ? <FaRegEye /> : <FaRegEyeSlash />}
+                  </button>
+                </div>
+                
+                <div className="flex items-center relative">
+                  <input
+                    id="confirmPassword"
+                    name="confirmPassword"
+                    type={showConfirmPassword ? "text" : "password"}
+                    value={confirmPassword}
+                    onChange={(e) => handleConfirmPassword(e.target.value)}
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    placeholder="Repetir contraseña"
+                    required
+                  />
+                  <button
+                    type="button"
+                    className="absolute right-0 pr-3 flex items-center top-1/2 transform -translate-y-1/2"
+                    onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                  >
+                    {showConfirmPassword ? <FaRegEye /> : <FaRegEyeSlash />}
+                  </button>
+                </div>
+                <div className="col-span-2 mt-2 mb-4">
+                  <ul className="text-sm text-gray-600">
+                    <li className={`flex items-center ${passwordConditions.length ? 'text-green-600' : ''}`}>
+                      {passwordConditions.length ? '✔' : '✘'} Al menos 8 caracteres
+                    </li>
+                    <li className={`flex items-center ${passwordConditions.uppercase ? 'text-green-600' : ''}`}>
+                      {passwordConditions.uppercase ? '✔' : '✘'} Contiene mayúsculas
+                    </li>
+                    <li className={`flex items-center ${passwordConditions.number ? 'text-green-600' : ''}`}>
+                      {passwordConditions.number ? '✔' : '✘'} Contiene números
+                    </li>
+                    <li className={`flex items-center ${passwordConditions.specialChar ? 'text-green-600' : ''}`}>
+                      {passwordConditions.specialChar ? '✔' : '✘'} Contiene caracteres especiales
+                    </li>
+                  </ul>
+                </div>
+
+              </div>
+              {!passwordsMatch && <p className="my-2 text-sm text-red-600">Las contraseñas no coinciden</p>}
+              {error && <label className="block text-red-600 mb-4">{error}</label>}
+              {success && <label className="block text-green-600 mb-4 text-center">{success}</label>}
+              <div className="text-center">
+                <input
+                  className="bg-sky-900 text-white px-4 py-2 rounded-md shadow-sm hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                  type="button"
+                  onClick={handleUpdate}
+                  value="Actualizar datos"
+                />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
