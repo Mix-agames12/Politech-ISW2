ed471fda7fbd2ff0ac85d157fc86dd67be0ae8b5
rarmendarizc <rossy.armendariz@epn.edu.ec>
2024-08-10 23:31:44 -0500

    Cambios

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,5 @@
 import React from 'react';
 import { FiMenu } from 'react-icons/fi';
-import { BiStore } from "react-icons/bi";
-import { GoArrowSwitch } from "react-icons/go";
-import { GoHome } from "react-icons/go";
-import { BiTable } from "react-icons/bi";
-
 
 export const Sidebar = ({ isOpen, toggleSidebar }) => {
   return (
