4105ec76af7bb17cd4d7438140a136c8af69493f
rarmendarizc <rossy.armendariz@epn.edu.ec>
2024-08-11 16:59:15 -0500

    App ajustado

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
 import PasswordReset from './components/PasswordReset';
@@ -10,26 +10,10 @@ import Movimientos from './components/Movimientos';
 import Home from './components/Home';
 import CrearCuenta from './components/CrearCuenta';
 import ForgotPassword from './components/ForgotPassword';
-import VerifyEmail from './components/VerifyEmail'; // Importar el componente VerifyEmail
+import VerifyEmail from './components/VerifyEmail';
 import BillsPayment from './components/BillsPayment';
 import GenerarPago from './components/GenerarPago';
 import { AuthProvider } from './context/AuthContext';
-import { useLocation } from 'react-router-dom';
-
-const AppRoutes = () => {
-  const location = useLocation();
-  const queryParams = new URLSearchParams(location.search);
-  const mode = queryParams.get('mode');
-  const oobCode = queryParams.get('oobCode');
-
-  if (mode === 'resetPassword' && oobCode) {
-    return <PasswordReset />;
-  } else if (mode === 'verifyEmail' && oobCode) {
-    return <VerifyEmail />;
-  } else {
-    return <Home />;
-  }
-};
 
 const App = () => {
   return (
@@ -45,6 +29,7 @@ const App = () => {
               <Route path="/transaction" element={<Transaction />} />
               <Route path="/gestionar-cuentas" element={<GestionarCuentas />} />
               <Route path="/movimientos" element={<Movimientos />} />
+              <Route path="/reset-password" element={<PasswordReset />} />
               <Route path="/crear-cuenta" element={<CrearCuenta />} />
               <Route path="/forgot-password" element={<ForgotPassword />} />
               <Route path="/verify-email" element={<VerifyEmail />} /> 
